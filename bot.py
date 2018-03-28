1d66a6e0aa0a5a61c67b0b569fcd70b855180942
vertauser-discord <37862602+vertauser-discord@users.noreply.github.com>
2018-03-28 09:30:53 +0200

    Mainfile for valle-bot

---
+++ bot.py
@@ -0,0 +1,44 @@
+#testbot
+
+from random import randint
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+bot = commands.Bot(command_prefix="va-")
+
+@bot.event
+async def on_ready():
+    print("READY")
+    print("ID: " + bot.user.id)
+    print("NAME: " + bot.user.name)
+
+@bot.command(pass_context=True)
+async def boti(ctx):
+    embed = discord.Embed(title="valle-bot info", description="Current version made 2018-03-23 (Version 1.2)", color=0x42e5f4)
+    embed.add_field(name="Made with :heart: in :flag_se: by VertaUser", value="Environment: discord.py")
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def number(ctx):
+    embed = discord.Embed(title="Random number", description=randint(0,99999), color=0x42e5f4)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def useri(ctx, user: discord.Member):
+    embed = discord.Embed(title="Info about {}".format(user.name), description="I found this about {}".format(user.name), color=0x42e5f4)
+    embed.add_field(name="User ID", value=user.id, inline=True)
+    embed.add_field(name="User joindate", value=user.joined_at, inline=True)
+    embed.add_field(name="User status", value=user.status)
+    embed.add_field(name="User role", value=user.top_role, inline=True)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def rndclr(ctx):
+    embed = discord.Embed(title="Random color", description=randint(0,255), color=0x42e5f4)
+    await bot.say(embed=embed)
+
+
+bot.run("NDIzNDAwMzQ5NzM1MzIxNjIw.DYpx0g.jFnoF0B-OH2WoBl76Fo_ipeabx4")
